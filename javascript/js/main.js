2f44b1d07fcc3bd9f6a350ee98a7e76fc9c13e60
vladimir kähri <vahermihkel@gmail.com>
2021-11-04 20:27:08 +0200

    bootstrap v2, javascript v1

---
+++ javascript/js/main.js
@@ -0,0 +1,46 @@
+
+var m = 10
+var m = 1000
+
+let x = 10
+//let x = 'zdfbd'
+// 
+// number
+// string 
+// undefined
+let y
+
+// NaN -> text
+// null
+// boolean
+// object
+
+if (x > 10){
+    // true
+} else if (y > 10){
+    // else if -> true
+} else {
+    // false
+}
+
+switch (x) {
+    case 5:
+        // todo 1
+        break;
+    case 10:
+        // todo 2
+        break;
+    default:
+        // todo
+        break;
+}
+
+let arr = ['audi', 'bmw', 'citoren', true, undefined, 10]
+        // index        true/false          increment/decrement
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element)
+    
+}
+
+console.log(arr[4])
