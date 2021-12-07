b522a95831e46186720479300778e7225d1c457f
vahermihkel <vahermihkel@gmail.com>
2021-12-07 20:18:31 +0200

    bookshelf node+express

---
+++ node-ts/backend/index.ts
@@ -0,0 +1,58 @@
+import { IBook } from './IBook'
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+//app.use(express.json()) // for parsing application/json
+app.use(bodyParser.json());
+//app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+let books:IBook[] = [
+    {
+        isbn:1111,
+        title:'Title 1',
+        description:'Description 1',
+        author:'Author 1',
+        year: 2000
+    },
+    {
+        isbn:2222,
+        title:'Title 2',
+        description:'Description 2',
+        author:'Author 2',
+        year: 2002
+    },
+    {
+        isbn:3333,
+        title:'Title 3',
+        description:'Description 3',
+        author:'Author 3',
+        year: 2003
+    }
+]
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+// method: GET, POST
+app.get('/books', function (req, res) {
+    res.send(books)
+})
+
+app.post('/newbook', function (req, res) {
+    console.log(req.body)
+    
+    books.push(req.body)
+
+    let response = {
+        status: 200,
+        result: 'Book is added successfully',
+        options: {}
+    }
+    res.status(200).json(response)
+})
+ 
+app.listen(3000, () => console.log('server ready'))
--- node-ts/frontend/js/main.js
+++ node-ts/frontend/js/main.js
@@ -2,6 +2,40 @@ let btnAddNewBook = document.getElementById('addNewBook')
 let modalWindow = document.getElementById('exampleModal')
 let modalWindowTitle = modalWindow.querySelector('#exampleModalLabel')
 let modalWindowBody = modalWindow.querySelector('.modal-body')
+let bookTitle = undefined;
+let bookDescription = undefined;
+
+let btnSaveChanges = document.getElementById('btnSaveChanges')
+
+btnSaveChanges.addEventListener('click', event => {
+    let form = document.getElementById('frmBook')
+    
+    var data = new FormData(form);
+    let tmp = []
+    for (var [input, value] of data) {
+        //console.log(input, value)
+        tmp[input] = value
+    }
+    let frmData = Object.assign({}, tmp)
+    console.log(frmData)
+    // step 2
+    fetch('http://localhost:3000/newbook', {
+        method: 'POST', 
+        mode: 'no-cors',
+        credentials: 'same-origin',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json',
+            //'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({"a": 1, "b": "Textual content"})
+    })
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data)
+        console.log(data.result)
+    })
+})
 
 
 btnAddNewBook.addEventListener('click', event => {
@@ -18,5 +52,38 @@ $('#exampleModal').on('show.bs.modal', function (event) {
         modalWindowTitle.innerText = 'New Book'
         modalWindowBody.innerHTML = form
     })
+})
+
+function generateCard(){
+    return `<div class="card m-3" style="width: 18rem;float: left;">
+    <div class="card-body">
+        <h5 class="card-title">${ bookTitle }</h5>
+        <p class="card-text">${ bookDescription }</p>
+        <a href="#" class="btn btn-primary">Edit</a>
+        <a href="#" class="btn btn-danger">Delete</a>
+        </div>
+    </div>`
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+    //console.log('body loaded')
+
+    fetch('http://localhost:3000/books')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let booksList = document.getElementById('books')
+            booksList.innerHTML = ''
+            data.forEach(book => {
+                bookTitle = book.title
+                bookDescription = `(isbn: ${ book.isbn})<br> 
+                <i>Description ${ book.description } </i><br>
+                <b>Author: ${ book.author } </b> <br>
+                Year: ${ book.year}
+                ` 
+                booksList.innerHTML += generateCard()
+
+            })
+        })
 })
 
